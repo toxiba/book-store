1fc68b5152bb8927ea9cafe1899b459c1a4203f3
toxiba <marcosvferretti@gmail.com>
2024-12-11 08:37:11 +0100

    adding login into the swagger documentation

--- back-end/src/main/java/com/example/bookstore/controllers/UserController.java
+++ back-end/src/main/java/com/example/bookstore/controllers/UserController.java
@@ -3,6 +3,10 @@ package com.example.bookstore.controllers;
 import com.example.bookstore.dtos.UserDto;
 import com.example.bookstore.mappers.UserMapper;
 import com.example.bookstore.services.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +33,25 @@ public class UserController {
     public UserDto register(@Valid @RequestBody UserDto user) {
         log.info("register({})", user.getUsername());
         return mapper.toDto(service.createUser(mapper.toEntity(user)));
+    }
+
+    @Operation(
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(example = "{ \"username\": \"string\", \"password\": \"string\" }")
+                    )
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Login successful"),
+                    @ApiResponse(responseCode = "401", description = "Invalid credentials")
+            }
+    )
+    @PostMapping("/login")
+    public void loginDocumentation() {
+        // For OpenAPI documentation only.
+        return;
     }
 
 }
